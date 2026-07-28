af2ad64224f7bf0b464cf043b6935f82a757c6bd
RafiAiub <rafiaiub01@gmail.com>
2026-07-27 22:41:53 -0300

    Primeiro commit

---
+++ Aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula02; 
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Rafi";
+        int number = 22;
+        char character = 'r';
+        bool boolean = true;
+
+        // existem varias outras variaveis
+
+        Console.WriteLine("Meu nome é" + myName + "e eu tenho" + number + "anos");
+    }
+}
---
+++ Aula03/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
