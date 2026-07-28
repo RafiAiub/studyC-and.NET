491bbdbc76ffe514d2417e0ebbd563c40c7a64da
RafiAiub <rafiaiub01@gmail.com>
2026-07-28 00:02:30 -0300

    Aula04-input-output

---
+++ Aula04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-----CADASTRO DE USUARIO-----");
+
+        //Solicitar o nome do usuario:
+        Console.WriteLine("Qual seu nome? ");
+        string name = Console.ReadLine();
+
+        //Solicitar idade do usuario:
+        Console.WriteLine("Digite sua idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("----------");
+
+        Console.WriteLine("Nome do usuario: " + name);
+        Console.WriteLine("Idade do usuario: " + age);
+    }
+}
