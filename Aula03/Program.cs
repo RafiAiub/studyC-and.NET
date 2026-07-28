f095fbe6a2d7c0e3f5702dc3170f6665bcb6fcd1
RafiAiub <rafiaiub01@gmail.com>
2026-07-27 23:34:47 -0300

    'Aula03-const'

--- Aula03/Program.cs
+++ Aula03/Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main()
     {
+        const int number = 1000;
 
+        Console.WriteLine("o valor é " + number)
     }
 }
+
+//const nao muda o valor igual variavel 
+//var
+//dynamic
+//obj
