c73d65dbdfa5b4432e65bb48008f2e7565ca4450
RafiAiub <rafiaiub01@gmail.com>
2026-07-28 21:53:57 -0300

    Aula06-if-else

---
+++ Aula06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula06
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-----Verificador de números pares e ímpares-----");
+            Console.WriteLine("Digite um número: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = number % 2;
+            if (result == 0)
+            {
+                Console.WriteLine("O numero digitado é par!");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar!");
+            }
+        }
+    }
+}
