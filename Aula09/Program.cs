bfb09cf07fb5ac03dc53336eba8bd650f7d438ce
RafiAiub <rafiaiub01@gmail.com>
2026-09-09 22:56:35 -0300

    Aula09-operadores-lógicos

---
+++ Aula09/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=====Operadores Lógicos=====");
+
+        bool isLogged = true;
+        bool hasAdminAcces = false;
+
+        Console.WriteLine("\nInformações do Usuario:");
+        Console.WriteLine("Usuario logado: " +  isLogged);
+        Console.WriteLine("Acesso ao painel de adm: " + hasAdminAcces);
+
+        //Operador (||) - OU
+        if(isLogged || hasAdminAcces)
+        {
+            Console.WriteLine("Usuario logado");
+        }
+        else
+        {
+            Console.WriteLine("Usuario nao esta logado");
+        }
+
+        //Operador (&&) - E
+        if (isLogged == true && hasAdminAcces == true)
+        {
+            Console.WriteLine("Acesso ao painel de adm concedido");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao painel adm negado");
+        }
+
+        //Operador (!) - Negação
+        if (!isLogged)
+        {
+            Console.WriteLine("Usuario nao esta logado");
+        }
+        else
+        {
+            Console.WriteLine("Usuario esta logado");
+        }
+    }
+}
