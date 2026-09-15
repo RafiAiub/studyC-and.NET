1aa32e4947db9cde9678c19f47bfcbd5740c1d37
RafiAiub <rafiaiub01@gmail.com>
2026-09-14 22:32:10 -0300

    Aula12-while

---
+++ Aula12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Ex1
+        //int i = 0;
+
+        //while (i <= 5)
+        //{
+        //    Console.WriteLine(i);
+        //    i++;
+        //}
+
+        //Ex2
+        double budget = 100;
+        while (budget > 0)
+        {
+            Console.WriteLine(budget);
+            //Realizando alguma operação
+            budget--;
+        }
+        Console.WriteLine("Saldo insuficiente");
+    }
+}
